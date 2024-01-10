710323476cab53dbd150dad1a409f37fcea73074
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-01-10 15:59:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-# Built-in imports
+from datetime import datetime
+import time
 
+
+# Part 1
+def clock(n):
+    # Your code here
+    pass
+
+
+# Part 2
+def lcm(a, b):
+    # Your code here
+    pass
+
+
+# Part 3
+def gcf(a, b):
+    # Your code here
+    pass
